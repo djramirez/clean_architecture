49932eeafdfa07aa5ee1a0d6d3eb77a35bc84121
David Jarkyn <jarkyn12@gmai.com>
2025-12-18 07:15:38 -0600

    Segunda sesion estructura de carpetas

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-import {healthCheck} from "@shared/health";
+//Test inicial
+//import {healthCheck} from "@shared/health";
+///const healthStatus = healthCheck();
+//console.log(`Health Status: ${healthStatus.status} at ${healthStatus.timestamp.toISOString()}`);
 
-const healthStatus = healthCheck();
-console.log(`Health Status: ${healthStatus.status} at ${healthStatus.timestamp.toISOString()}`);
+import { buildServer } from "@infrastructure/http/server";
+
+const port = Number(process.env.PORT) ?? 3000;
+buildServer().then(app => app.listen({port}));
+
